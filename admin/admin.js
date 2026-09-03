ffafc689e2215fa237188fd35a358c14deb20f8f
pulungrwo <pulungrwo@gmail.com>
2026-09-03 19:48:35 +0700

    auto(admin): deploy 1.2.6

--- admin/admin.js
+++ admin/admin.js
@@ -1,7 +1,7 @@
 (() => {
   "use strict";
 
-  const ADMIN_BUILD = "1.2.5";
+  const ADMIN_BUILD = "1.2.6";
   const config = window.PROXYZ_ADMIN_CONFIG || {};
 
   async function checkAdminBuild() {
